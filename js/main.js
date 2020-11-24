6f92163705c08d17ae97ab625d0467fa340ba473
Simona Cinti <simona.cinti2@gmail.com>
2020-11-24 15:00:19 +0100

    print array without style

--- js/main.js
+++ js/main.js
@@ -8,6 +8,19 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
-    }
+       cds: [],
+    }, // end Data
+    created(){
+        // Get  Cd List
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then(result => {
+                // Copy of the cd list 
+                this.cds = result.data.response;
+                
+            })
+            .catch(error => {
+                // handle error
+                console.log(error);
+            })
+    }, // end Created
 })
