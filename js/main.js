19b383cd84cdfac51af2ff7c3f86cfdfd0fe28f2
Simona Cinti <simona.cinti2@gmail.com>
2020-11-24 15:57:38 +0100

    filter by call to db

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ var app = new Vue({
     el: '#app',
     data: {
        cds: [],
+       selectedGenre: 'all',
     }, // end Data
     created(){
         // Get  Cd List
@@ -23,4 +24,23 @@ var app = new Vue({
                 console.log(error);
             })
     }, // end Created
+    methods: {
+        filterGenre(){
+            axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+                .then(result => {
+                    // call to db for cd list
+                    let cdList = result.data.response;
+                    // check genre select plus filter
+                    if (this.selectedGenre !== 'all'){
+                        cdList = cdList.filter(cd =>cd.genre.toLowerCase() === this.selectedGenre);                        
+                    }
+                    // return result
+                    this.cds = cdList;
+                })
+                .catch(error => {
+                    // handle error
+                    console.log(error);
+                })
+        }
+    } // end Methods
 })
